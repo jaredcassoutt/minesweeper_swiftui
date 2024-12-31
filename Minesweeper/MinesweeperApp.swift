e8bc67259ab2b6445b4b93b4da85c79c6392289d
jaredcassoutt <jaredcassoutt@gmail.com>
2024-12-31 10:28:44 -0800

    Initial Commit

---
+++ Minesweeper/MinesweeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinesweeperApp.swift
+//  Minesweeper
+//
+//  Created by Jared Cassoutt on 12/31/24.
+//
+
+import SwiftUI
+
+@main
+struct MinesweeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MinesweeperTests/MinesweeperTests.swift
@@ -0,0 +1,17 @@
+//
+//  MinesweeperTests.swift
+//  MinesweeperTests
+//
+//  Created by Jared Cassoutt on 12/31/24.
+//
+
+import Testing
+@testable import Minesweeper
+
+struct MinesweeperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
